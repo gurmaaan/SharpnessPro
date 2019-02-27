4109fa2ddecf13b0c1a2c4e5223097e9fee7be3d
Dmitry <dmitry@gurov.co>
2019-02-27 19:47:21 +0300

    Добавлен зум

--- dialogs/mainwindow.cpp
+++ dialogs/mainwindow.cpp
@@ -24,6 +24,8 @@ void MainWindow::setupWidgets()
 {
     _scene = new QGraphicsScene;
     ui->graphicsView->setScene(_scene);
+    ui->zoom_V_lyt->setAlignment(Qt::AlignHCenter);
+    setScaleRatio(1.0);
 }
 
 void MainWindow::on_actionOpenImg_triggered()
@@ -52,4 +54,50 @@ QImage MainWindow::baseImg() const
 void MainWindow::setBaseImg(const QImage &baseImg)
 {
     _baseImg = baseImg;
+}
+
+void MainWindow::on_actionZoomIn_triggered()
+{
+    double newScaleRatio = scaleRatio() + 0.15;
+    setScaleRatio(newScaleRatio);
+    int newSliderValue = static_cast<int>(newScaleRatio) * 100;
+    ui->zoom_sldr->setValue(newSliderValue);
+}
+
+void MainWindow::on_actionZoomOut_triggered()
+{
+    double newScaleRatio = scaleRatio() - 0.15;
+    setScaleRatio(newScaleRatio);
+    int newSliderValue = static_cast<int>(newScaleRatio) * 100;
+    ui->zoom_sldr->setValue(newSliderValue);
+}
+
+void MainWindow::on_zoom_sldr_sliderMoved(int position)
+{
+    double dPosition = static_cast<double>(position) / 100.0;
+    setScaleRatio(dPosition);
+}
+
+double MainWindow::scaleRatio() const
+{
+    return _scaleRatio;
+}
+
+void MainWindow::setScaleRatio(double scaleRatio)
+{
+    _scaleRatio = scaleRatio;
+    ui->zoom_sb->setValue(scaleRatio);
+}
+
+void MainWindow::on_zoom_sb_valueChanged(double arg1)
+{
+    _scene->clear();
+    QImage bi = baseImg();
+    double newWd = static_cast<double>(bi.width()) * arg1;
+    double newHd = static_cast<double>(bi.height()) * arg1;
+    int newW = static_cast<int>(newWd);
+    int newH = static_cast<int>(newHd);
+    bi = bi.scaled(newW, newH);
+    QGraphicsPixmapItem *pmItem = new QGraphicsPixmapItem(QPixmap::fromImage(bi));
+    _scene->addItem(pmItem);
 }
--- dialogs/mainwindow.h
+++ dialogs/mainwindow.h
@@ -25,13 +25,21 @@ public:
     QImage baseImg() const;
     void setBaseImg(const QImage &baseImg);
 
+    double scaleRatio() const;
+    void setScaleRatio(double scaleRatio);
+
 private slots:
     void on_actionOpenImg_triggered();
     void on_actionExit_triggered();
+    void on_actionZoomIn_triggered();
+    void on_actionZoomOut_triggered();
+    void on_zoom_sldr_sliderMoved(int position);
+    void on_zoom_sb_valueChanged(double arg1);
 
 private:
     Ui::MainWindow *ui;
     QGraphicsScene *_scene;
+    double _scaleRatio;
     //
     QImage _baseImg;
 
